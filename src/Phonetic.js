0ef571b3341609399974bf6d58d13d3392bada6d
AmyLynn87 <e.crescentini87@gmail.com>
2021-06-28 11:32:43 +0200

    put an audio button for the phonetic and styled it. styled also the synonyms

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,15 +3,16 @@ import "./Phonetic.css";
 import ReactAudioPlayer from 'react-audio-player';
 
 export default function Phonetic(props) {
+let audioElement = new Audio(props.phonetic.audio);
+
+function play() {
+    audioElement.play();
+}
+
     return (
         <div className="Phonetic">
+            <button className="audioButton" onClick={play}><i class="fas fa-volume-up"></i></button>
              <div className="phoneticText">{props.phonetic.text}</div>
-             <div className="audioPlayer">
-            <ReactAudioPlayer
-  src={props.phonetic.audio}
-  controls
-/> 
-</div>
 </div>
     );
 }
